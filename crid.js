52dbeedd9ca1d5178ba4da9ac412af925fb31595
Premkumar163 <premkumar829107@gmail.com>
2025-07-03 21:05:16 +0530

    update

--- crid.js
+++ crid.js
@@ -11,11 +11,12 @@ let  fetchdata=async()=>{
 
   
   
- let res= await fetch(url,{method:"GET"})
+ let res=await fetch(url)
 
- let data = await res.json()
+ let data=await res.json()
 
  console.log(data);
+ 
  let show=document.querySelector("#datashow")
 
 data.map((e)=>{
@@ -32,7 +33,7 @@ data.map((e)=>{
     <td>${e.ReturnDate}</td>
     <td>${e.Passengers * e.price }</td>
     <td onclick="Del('${e.id}')"> Delete </td>
-    <td onclick="upd('${e.id}')"> Update </td>
+    <td onclick="formopen('${e.id}')"> Update </td>
     
     </tr>`
  })
@@ -62,7 +63,7 @@ let Ins=()=>{
   
   let url='http://localhost:3000/Flynow'
 
-  fetch(url , {
+  fetch( url , {
 
     method:"POST" ,
     headers:{
@@ -97,15 +98,150 @@ let Ins=()=>{
 
 }
 
-let upd=async(id)=>{
+let formopen=async(id)=>{
 
-   let url='http://localhost:3000/Flynow'
+   let url=`http://localhost:3000/Flynow/${id}`
 
   
   
- let res= await (url,{method:"GET"})
+ let res= await fetch(url,{method:"GET"})
 
  let data = await res.json()
 
+ console.log(data)
+
+ let FormData= `
+       
+
+
+<div id="BookingS">
+
+  <div class="flight-booking-section">
+    <h2>Book Your Flight</h2>
+    <form id="flightSearchForm">
+      <div class="form-row">
+        <div class="form-group">
+          <label for="tripType">Trip Type</label>
+          <select id="uptripType" value="${data.TripType}"  name="tripType">
+            <option value="roundtrip">Round Trip</option>
+            <option value="oneway">One Way</option>
+            <option value="multi">Multi-City</option>
+          </select>
+        </div>
+        
+        <div class="form-group">
+          <label for="passengers">Passengers</label>
+          <select id="uppassengers" value="${data.Passengers}" name="passengers">
+            <option value="1">1 Adult</option>
+            <option value="2">2 Adults</option>
+            <option value="3">3 Adults</option>
+            <option value="4">4 Adults</option>
+            <option value="family">Family (2 Adults + 2 Children)</option>
+          </select>
+        </div>
+        
+        <div class="form-group">
+          <label for="cabinClass">Cabin Class</label>
+          <select id="upcabinClass"value="${data.CabinClass}" name="cabinClass">
+            <option value="economy">Economy</option>
+            <option value="premium">Premium Economy</option>
+            <option value="business">Business</option>
+            <option value="first">First Class</option>
+          </select>
+        </div>
+      </div>
+      
+      <div class="form-row">
+        <div class="form-group">
+          <label for="departureAirport">From</label>
+          <input type="text" id="updepartureAirport" value="${data.From}" name="departureAirport" placeholder="City or Airport">
+        </div>
+        
+        <div class="form-group">
+          <label for="arrivalAirport">To</label>
+          <input type="text" id="uparrivalAirport" value="${data.To}" name="arrivalAirport" placeholder="City or Airport">
+        </div>
+      </div>
+      
+      <div class="form-row">
+        <div class="form-group">
+          <label for="departureDate">Departure Date</label>
+          <input type="date" id="updepartureDate" value="${data.DepartureDate}" name="departureDate">
+        </div>
+        
+        <div class="form-group return-date-group">
+          <label for="returnDate">Return Date</label>
+          <input type="date" id="upreturnDate" value="${data.ReturnDate}" name="returnDate">
+        </div>
+      </div>
+      
+      <div class="form-row">
+        <input type="Submit" value="Update Now" class="search-flights-btn" onclick="return UpdateData('${data.id}')"></button>
+      </div>
+    </form>
+  </div>
+  
+ 
+ 
+
+</div>
+</section>
+
+`
+
+document.querySelector("#formshow").innerHTML=FormData
+
 
 }
+
+let UpdateData=(id)=>{
+
+  let tripType=document.querySelector("#uptripType").value
+  let passengers=document.querySelector("#uppassengers").value 
+  let cabinClass=document.querySelector("#upcabinClass").value
+  let form=document.querySelector("#updepartureAirport").value
+  let to=document.querySelector("#uparrivalAirport").value
+  let departureDate=document.querySelector("#updepartureDate").value
+  let returnDate=document.querySelector("#upreturnDate").value
+
+  let url=`http://localhost:3000/Flynow/${id}`
+
+  fetch(url,{
+
+    method:"PUT" ,
+    headers:{
+
+      "Content-type":"application/json"
+    },
+    body: JSON.stringify( {
+
+      TripType:tripType,
+      Passengers:passengers,
+      CabinClass:cabinClass,
+      From:form,
+      To:to,
+      DepartureDate:departureDate,
+      ReturnDate:returnDate,
+      price:1000
+
+
+
+
+
+
+
+    })
+
+
+  })
+
+  location,href="booking.html"
+  return false
+
+
+}
+
+
+
+
+
